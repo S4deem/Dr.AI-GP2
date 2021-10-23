61ef877ecd8b6e545e987d3ac65e33a902539653
S4deem <S44deem@gmail.com>
2021-10-23 22:12:39 +0300

    GP2

--- app/src/main/java/com/dr/ai/drai_2/doctorsAppointments.java
+++ app/src/main/java/com/dr/ai/drai_2/doctorsAppointments.java
@@ -26,14 +26,14 @@ import java.util.Calendar;
 import java.util.Locale;
 
 public class doctorsAppointments extends AppCompatActivity {
-    private DatePickerDialog datePickerDialog;
-    private Button dateButton;
-    Button timeButton;
-    int hour, minute;
+
+
     private Spinner spinnerTextSize;
-    private Button doctorsAppointmentsButton;
+    private Button doctorAppointments;
+
     private NavigationView mainNavView;
     private Menu mainNavMenu;
+
     private MenuItem menuItem;
     private Button menuButton;
     private DrawerLayout drawer_layout;
@@ -51,7 +51,7 @@ public class doctorsAppointments extends AppCompatActivity {
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         return true;
                     case R.id.goBack:
-                        startActivity(new Intent(getApplicationContext(), doctorsScreen.class));
+                        startActivity(new Intent(getApplicationContext(), patientAppointments.class));
                         return true;
                 }
                 return false;
@@ -156,134 +156,32 @@ public class doctorsAppointments extends AppCompatActivity {
                 return false;
             }
         });
-        initDatePicker();
-        dateButton = findViewById(R.id.buttonDate);
-        dateButton.setText(getTodaysDate());
-        timeButton = findViewById(R.id.buttonTime);
-        spinnerTextSize = findViewById(R.id.doctorSpinner);
-        String[] textSizes = getResources().getStringArray(R.array.Patient_Id);
+
+
+        spinnerTextSize = findViewById(R.id.patientSpinner);
+        String [] textSizes = getResources().getStringArray(R.array.Patient_Id);
+
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, textSizes);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerTextSize.setAdapter(adapter);
-        doctorsAppointmentsButton = findViewById(R.id.doctorsAppointmentsButton);
-        doctorsAppointmentsButton.setOnClickListener(new View.OnClickListener() {
+
+        doctorAppointments = findViewById(R.id.doctorAppointments);
+        doctorAppointments.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                opendoctorsAppointmentsPage();
+
+                openDoctorsAppointments();
             }
         });
     }
 
-    private String getTodaysDate() {
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        month = month + 1;
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        return makeDateString(day, month, year);
-    }
-
-
-    private void initDatePicker() {
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month + 1;
-                String date = makeDateString(day, month, year);
-                dateButton.setText(date);
-
-
-            }
-        };
-
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        int style = AlertDialog.THEME_HOLO_LIGHT;
-        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
-
-    }
-
-    private String makeDateString(int day, int month, int year) {
-        return getMonthFormat(month) + "  " + day + "  " + year;
-
-    }
-
-    private String getMonthFormat(int month) {
-        if (month == 1)
-            return "JAN";
-
-        if (month == 2)
-            return "FEB";
-
-        if (month == 3)
-            return "MAR";
-
-        if (month == 4)
-            return "APR";
-
-        if (month == 5)
-            return "MAY";
-
-        if (month == 6)
-            return "JUN";
-
-        if (month == 7)
-            return "JUL";
-
-        if (month == 8)
-            return "AUG";
-
-        if (month == 9)
-            return "SEP";
-
-        if (month == 10)
-            return "OCT";
-
-        if (month == 11)
-            return "NOV";
-
-        if (month == 12)
-            return "DEC";
-
-        return "JAN";
-
-    }
-
-    public void openDatePicker(View view) {
-        datePickerDialog.show();
-
-    }
-
-    public void popTimePicker(View view) {
-        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int selectHour, int selectMinute) {
-
-                hour = selectHour;
-                minute = selectMinute;
-                timeButton.setText(String.format(Locale.getDefault(), "%2d:%2d", hour, minute));
-            }
-        };
-
-        int style = AlertDialog.THEME_HOLO_DARK;
-
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minute, true);
-        timePickerDialog.setTitle("Select Time");
-        timePickerDialog.show();
-
-    }
-
-    public void opendoctorsAppointmentsPage() {
+    public void openDoctorsAppointments(){
         Intent intent = new Intent(this, doctorsAppointments1.class);
         startActivity(intent);
     }
-
     private void menuButton() {
 
         drawer_layout.openDrawer(GravityCompat.START);
 
     }
-
 }
